2c4810e1834ce8582a23efe4d7bfc718dd17cb4c
Bas Mols <bas.mols@electude.com>
2026-05-19 10:46:07 +0200

    rebuild camera script

--- camera.gd
+++ camera.gd
@@ -1,27 +1,26 @@
 extends Camera2D
 
 var follow: Node2D = null
-var horse: Node2D = null
-var character: Node2D = null
+var current_index: int = 0
+
+@export var targets: Array[Node2D] = []
 
 func _ready() -> void:
-	get_node("control/hFlowContainer/switchButton").pressed.connect(on_switch_button_pressed)
-	horse = get_parent().get_node("horse")
-	character = get_parent().get_node("character")
-	set_follow_target("character")
+	if targets.size() == 0:
+		push_error("Camera has no target assigned.")
+		set_process(false)
 
-func set_follow_target(target: String) -> void:
-	if target == "horse": 
-		follow = horse
-		character.set_meta("camera_follow", false)
-		horse.set_meta("camera_follow", true)
-	else:
-		follow = character
-		character.set_meta("camera_follow", true)
-		horse.set_meta("camera_follow", false)
+	switch_follow_target(0)
 
-func on_switch_button_pressed() -> void:
-	set_follow_target("character" if follow == horse else "horse")
+func switch_follow_target(index: int) -> void:
+	follow = targets[index]
+	for i in targets.size():
+		targets[i].set_meta("camera_follow", i == index)
+	current_index = index
+
+func cycle_follow_target() -> int:
+	switch_follow_target((current_index + 1) % targets.size())
+	return current_index
 
 func _process(_delta: float) -> void:
 	global_position.x = follow.global_position.x
---
+++ switch_button.gd
@@ -0,0 +1,17 @@
+extends Button
+
+@export var camera: Camera2D = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if camera == null:
+		push_error("Switch button has no camera assigned.")
+		set_process(false)
+
+	pressed.connect(_button_pressed)
+	text = "1"
+
+
+func _button_pressed() -> void:
+	var index = camera.cycle_follow_target()
+	text = str(index + 1)
